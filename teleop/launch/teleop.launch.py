5ef02a53f53ad79479de421fb65d7f7e42f2f6e2
Tanmay Gujar <tanmaygujar999@gmail.com>
2021-09-16 13:33:04 -0700

    added param file

--- teleop/launch/teleop.launch.py
+++ teleop/launch/teleop.launch.py
@@ -1,18 +1,29 @@
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+
+    config = os.path.join(
+        get_package_share_directory('teleop'),
+        'config',
+        'params.yaml'
+    )
+
     return LaunchDescription([
         Node(
             package='teleop',
             namespace='teleop',
             executable='teleop_node',
-            name='teleop_node'
+            name='teleop_node',
+            parameters = [config]
         ),
         Node(
             package='joy',
             namespace='joy',
             executable='joy_node',
-            name='joy_node'
+            name='joy_node',
+            parameters= [config]
         )
     ])
