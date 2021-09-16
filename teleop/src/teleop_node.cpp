33fe08c0090701501486f8d71825aac8bf99e008
Tanmay Gujar <tanmaygujar999@gmail.com>
2021-09-16 15:59:09 -0700

    commented out code for changing gears

--- teleop/src/teleop_node.cpp
+++ teleop/src/teleop_node.cpp
@@ -16,23 +16,31 @@ public:
         publisher_ = this->create_publisher<lgsvl_msgs::msg::VehicleControlData>("/lgsvl/vehicle_control_cmd", 10);
 
         // ref: http://docs.ros.org.ros.informatik.uni-freiburg.de/en/galactic/Tutorials/Using-Parameters-In-A-Class-CPP.html
+        /*
+            Note: Stuff related to changing gears has been commented because its not 
+            yet supported for lgsvl :(
+            
+            ref: https://github.com/lgsvl/simulator/issues/1303
+        */
+
         this->declare_parameter("accel", 5);
         this->declare_parameter("braking", 5);
         this->declare_parameter("wheel", 0);
-        this->declare_parameter("drive", 3);
         this->declare_parameter("estop", 0);
-        this->declare_parameter("parking", 1);
-        this->declare_parameter("reverse", 0);
-        this->declare_parameter("low", 2);
+        // this->declare_parameter("drive", 3);
+        // this->declare_parameter("parking", 1);
+        // this->declare_parameter("reverse", 0);
+        // this->declare_parameter("low", 2);
         this->declare_parameter("target_angular_rate", 40);
 
         this->get_parameter("accel", accel);
         this->get_parameter("braking", braking);
         this->get_parameter("wheel", wheel);
-        this->get_parameter("drive", drive);
-        this->get_parameter("parking", parking);
-        this->get_parameter("reverse", reverse);
-        this->get_parameter("low", low);
+        this->get_parameter("estop", estop);
+        // this->get_parameter("drive", drive);
+        // this->get_parameter("parking", parking);
+        // this->get_parameter("reverse", reverse);
+        // this->get_parameter("low", low);
         this->get_parameter("target_angular_rate", target_angular_rate);
     }
 
@@ -44,7 +52,7 @@ private:
         vehicle_control.braking_pct = map_linear<double>(1, -1, 0, 1, msg->axes[braking]);
         vehicle_control.target_wheel_angle = map_linear<double>(1, -1, - M_PI/2 , M_PI / 2, msg->axes[wheel]);
         vehicle_control.target_wheel_angular_rate = target_angular_rate;
-        vehicle_control.target_gear = get_gear(msg);
+        vehicle_control.target_gear = 1 //get_gear(msg);  
         publisher_->publish(vehicle_control);
     }
 
@@ -53,20 +61,21 @@ private:
         return ((val - ip_start)/ (ip_end - ip_start)) * (op_end - op_start) + op_start;
     }
 
-    int get_gear(const sensor_msgs::msg::Joy::SharedPtr msg) const {
-        if (msg->buttons[drive]) {
-            return 1;
-        } else if (msg->buttons[reverse]) {
-            return 2;
-        } else if (msg->buttons[parking]) {
-            return 3;
-        } else if (msg->buttons[low]) {
-            return 4;
-        }
-        return 0;
-    }
+    // int get_gear(const sensor_msgs::msg::Joy::SharedPtr msg) const {
+    //     if (msg->buttons[drive]) {
+    //         return 1;
+    //     } else if (msg->buttons[reverse]) {
+    //         return 2;
+    //     } else if (msg->buttons[parking]) {
+    //         return 3;
+    //     } else if (msg->buttons[low]) {
+    //         return 4;
+    //     }
+    //     return 0;
+    // }
 
-    int accel, braking, wheel, drive, estop, parking, reverse, low, target_angular_rate;
+    int accel, braking, wheel, target_angular_rate, estop; // control vars
+    // int drive, parking, reverse, low; // vars for gears
     rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
     rclcpp::Publisher<lgsvl_msgs::msg::VehicleControlData>::SharedPtr publisher_;
 };
