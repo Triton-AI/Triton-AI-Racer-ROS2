fd35c1af613dcf816f76ef7d5053f51d3a41a672
Haoru Xue <hxue@ucsd.edu>
2021-11-11 15:31:47 -0800

    fix script

--- deploy/model_190/launch_dev.sh
+++ deploy/model_190/launch_dev.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-docker run -it --rm --update --name tai-racer-ros2-190 \
+docker pull haoru233/triton-ai-racer-ros2:arm64-galactic-190
+docker run -it --rm --name tai-racer-ros2-190 \
     --privileged \
     --net=host \
     --mount type=bind,source="$(pwd)"/../../src,target=/root/ros2_ws/src \
--- deploy/model_190/launch_latest.sh
+++ deploy/model_190/launch_latest.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+docker pull haoru233/triton-ai-racer-ros2:arm64-galactic-190
 docker run -it --rm --update --name tai-racer-ros2-190 \
     --privileged \
     --net=host \
