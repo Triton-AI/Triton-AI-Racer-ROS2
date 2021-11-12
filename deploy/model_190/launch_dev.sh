a8a0eefaf70a7a033963d2614ed1fff99dc1824b
Haoru Xue <hxue@ucsd.edu>
2021-11-11 16:30:37 -0800

    python deps in Dockerfile

--- deploy/model_190/launch_dev.sh
+++ deploy/model_190/launch_dev.sh
@@ -3,5 +3,5 @@ docker pull haoru233/triton-ai-racer-ros2:arm64-galactic-190
 docker run -it --rm --name tai-racer-ros2-190 \
     --privileged \
     --net=host \
-    --mount type=bind,source="$(pwd)"/../../src,target=/root/ros2_ws/src \
+    --mount type=bind,source=/home/jetson/haoru/dev,target=/root/ros2_ws/src \
     haoru233/triton-ai-racer-ros2:arm64-galactic-190
